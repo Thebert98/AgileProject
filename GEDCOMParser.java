43d1a85b147f81c03af98f9a0f3af1a2e2af1120
Thebert98 <t.heb1998@gmail.com>
2023-02-15 02:31:20 -0500

    Update GEDCOMParser.java
    Added more information to output and made it so code takes the ged filename as a command line arg

--- GEDCOMParser.java
+++ GEDCOMParser.java
@@ -9,7 +9,8 @@ import java.util.Map;
 public class GEDCOMParser {
 
     public static void main(String[] args) {
-        String fileName = "/home/krinal/Desktop/Agile/Project01.ged"; // replace with actual file name
+        String fileName = args[0];
+        //String fileName = "C:\\Users\\theb1\\Documents\\CS555\\AgileProject-main\\family.ged"; // replace with actual file name
         List<Individual> individuals = new ArrayList<>();
         List<Family> families = new ArrayList<>();
 
@@ -19,9 +20,10 @@ public class GEDCOMParser {
             Family currentFamily = null;
             Map<String, Individual> individualsMap = new HashMap<>();
             Map<String, Family> familiesMap = new HashMap<>();
-
+            String tag =null;
             while ((line = br.readLine()) != null) {
                 String[] tokens = line.split(" ");
+
 
                 if (tokens[0].equals("0")) {
                     if (tokens.length >= 3 && tokens[2].equals("INDI")) {
@@ -35,9 +37,27 @@ public class GEDCOMParser {
                     }
                 } else if (tokens[0].equals("1")) {
                     switch (tokens[1]) {
-                        case "NAME":
+                        case "SEX":
                             if (currentIndividual != null) {
-                                currentIndividual.setName(tokens[2]);
+                                currentIndividual.setSex(tokens[2]);
+                            }
+                            break;
+                        case "DEAT":
+                            if (currentIndividual != null) {
+                                currentIndividual.setAlive("N");
+
+                                tag = "DEAT";
+
+                            }
+                            break;
+                        case "BIRT":
+                            if (currentIndividual != null) {
+                                tag = "BIRT";
+                            }
+                            break;
+                        case "MARR":
+                            if (currentFamily != null) {
+                                tag = "MARR";
                             }
                             break;
                         case "HUSB":
@@ -49,12 +69,59 @@ public class GEDCOMParser {
                             if (currentFamily != null) {
                                 currentFamily.setWife(individualsMap.get(tokens[2]));
                             }
+                        case "_CURRENT":
+                            if (currentFamily != null) {
+                                if(tokens[2]=="Y") {
+                                    currentFamily.setDivorce("N");
+                                }
+                                 else {
+                                    currentFamily.setDivorce("Y");
+                                }
+                            }
                             break;
-                        // case "CHIL":
-                        // if (currentFamily != null) {
-                        // currentFamily.addChild(individualsMap.get(tokens[2]));
-                        // }
-                        // break;
+                         case "CHIL":
+                             if (currentFamily != null) {
+                             currentFamily.addChild(individualsMap.get(tokens[2]));
+                             }
+                             break;
+                    }
+                }
+                else if (tokens[0].equals("2")) {
+                    switch (tokens[1]) {
+                        case "GIVN":
+                            if (currentIndividual != null) {
+                                currentIndividual.setName(tokens[2] );
+
+                            }
+                            break;
+                        case "SURN":
+                            if (currentIndividual != null) {
+                                currentIndividual.setLastName(tokens[2]);
+                            }
+                            break;
+                        case "DATE":
+                            if (currentIndividual != null) {
+
+                                if(tag != null) {
+
+                                    if(tag == "BIRT") {
+                                        currentIndividual.setBirthday(tokens[2] + "-" + tokens[3] + "-" + tokens[4]);
+
+                                        tag = null;
+                                    }
+                                    else if(tag == "DEAT"){
+                                        currentIndividual.setDeathDate(tokens[2] + "-" + tokens[3] + "-" + tokens[4]);
+
+                                        tag = null;
+                                    }
+                                    else if(tag == "MARR"){
+                                        currentFamily.setMarriageDate(tokens[2] + "-" + tokens[3] + "-" + tokens[4]);
+
+                                        tag = null;
+                                    }
+                                }
+                            }
+                            break;
                     }
                 }
             }
@@ -64,20 +131,29 @@ public class GEDCOMParser {
 
         System.out.println("Individuals:");
         for (Individual individual : individuals) {
-            System.out.println(individual.getId() + " " + individual.getName());
+            System.out.println("INDI:" + individual.getId() + " NAME:" + individual.getName() + " SEX:" + individual.getSex()
+            + " BIRTH:" + individual.getBirthday() + " ALIVE:" + individual.getAlive() + " DEATH:" + individual.getDeathDate()+"\n"
+            );
         }
 
         System.out.println("Families:");
         for (Family family : families) {
-            System.out.println(family.getId() + " " + family.getHusband().getName() + " and "
-                    + family.getWife().getName());
+            System.out.println("FAM:"+family.getId() + " MARR:" + family.getMarriageDate() + " DIV:" + family.getDivorce() +
+                    " HUSB:" + family.getHusband().getId() + " " + family.getHusband().getName() + " WIFE:"
+                    +family.getWife().getId() + " " +family.getWife().getName() + " CHIL:" + family.getChildren() +"\n");
         }
     }
 }
 
 class Individual {
-    private String id;
+    public String id;
     private String name;
+    private String lastName;
+    private String sex;
+    private String alive = "Y";
+    private String birthday;
+    private String deathDate = "NA";
+
 
     public Individual(String id) {
         this.id = id;
@@ -88,11 +164,47 @@ class Individual {
     }
 
     public String getName() {
-        return name;
+        return name + " " + lastName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public String getAlive() {
+        return alive;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public String getDeathDate() {
+        return deathDate;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void setAlive(String alive) {
+        this.alive = alive;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public void setDeathDate(String deathDate) {
+        this.deathDate = deathDate;
     }
 }
 
@@ -100,7 +212,9 @@ class Family {
     private String id;
     private Individual husband;
     private Individual wife;
-    // private List<Individual> children = new ArrayList<>();
+    private String marriageDate;
+    private String divorce;
+    private ArrayList<Individual> children = new ArrayList<>();
 
     public Family(String id) {
         this.id = id;
@@ -110,19 +224,52 @@ class Family {
         return id;
     }
 
-    public Individual getHusband() {
-        return husband;
+    public String getMarriageDate() {
+        return marriageDate;
     }
 
-    public void setHusband(Individual husband) {
-        this.husband = husband;
+    public String getDivorce() {
+        return divorce;
+    }
+
+    public Individual getHusband() {
+        return husband;
     }
 
     public Individual getWife() {
         return wife;
     }
 
+    public String getChildren() {
+        String childrenIds = "{";
+        for(int i = 0;i < children.size();i++){
+            if(i != 0){
+                childrenIds = childrenIds +"," + children.get(i).id;
+            }
+            childrenIds = childrenIds + children.get(i).id;
+        }
+        childrenIds = childrenIds + "}";
+        return childrenIds;
+    }
+
+    public void setHusband(Individual husband) {
+        this.husband = husband;
+    }
+
     public void setWife(Individual wife) {
         this.wife = wife;
     }
+
+    public void setMarriageDate(String marriageDate) {
+        this.marriageDate = marriageDate;
+    }
+
+    public void setDivorce(String divorce) {
+        this.divorce = divorce;
+    }
+
+    public void addChild(Individual child) {
+        this.children.add(child);
+    }
+
 }
